cbffad89280169f486e774ee455ea4390372a1f2
renzuzu <82306584+renzuzu@users.noreply.github.com>
2023-01-19 21:57:25 +0800

    tweak(conf): added item names

--- config.lua
+++ config.lua
@@ -6,7 +6,7 @@ Config.weight = 1.5
 Config.jobonly = false
 Config.turbojob = 'mechanic'
 Config.turbos = {
-	Street = {Power = 1.2, Torque = 1.3, value = 25000},
-	Sports = {Power = 1.4, Torque = 1.5, value = 55000},
-	Racing = {Power = 2.2, Torque = 2.3, value = 125000},
+	Street = {Power = 1.2, Torque = 1.3, value = 25000, item = 'turbostreet'},
+	Sports = {Power = 1.4, Torque = 1.5, value = 55000, item = 'turbosports'},
+	Racing = {Power = 2.2, Torque = 2.3, value = 125000, item = 'turboracing'},
 }
